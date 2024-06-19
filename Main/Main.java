795df98a36e0357ab830d3ab0d289edd94e57412
StanislausJoel <stanislausjoel.fa@gmail.com>
2024-06-19 10:42:10 +0700

    add main

---
+++ Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+import java.util.ArrayList;
+import Model.*;
+
+public class Main {
+    
+    public static ArrayList<Paket> listPaket = new ArrayList<>();
+
+    public static void main(String[] args) {
+        
+
+
+    }
+
+}
